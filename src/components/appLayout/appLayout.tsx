85c41f26d3065a53a255db0c0c7e0043e185f1f0
Leonardo Franca Almeida Silva <leofrancal17@gmail.com>
2025-05-20 16:15:35 -0300

    usePathName

---
+++ src/components/appLayout/appLayout.tsx
@@ -0,0 +1,21 @@
+// components/layout/AppLayout.tsx
+"use client";
+
+import { usePathname } from "next/navigation";
+import Header from "../header/header";
+import Footer from "../footer/footer";
+import ParticlesBackground from "../particlesBackground/particlesBackground";
+
+export default function AppLayout({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+  const hideLayout = pathname === "/loginPage" || pathname === "/registerPage";
+
+  return (
+    <>
+      {!hideLayout && <ParticlesBackground />}
+      {!hideLayout && <Header />}
+      {children}
+      {!hideLayout && <Footer />}
+    </>
+  );
+}
